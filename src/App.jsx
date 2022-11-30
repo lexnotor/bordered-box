fdd2bc15469f3c0b80afe18f026c379e37308674
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    "Initial commit"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-orange-900 text-7xl uppercase flex justify-center items-center min-h-screen">
+      <span className="text-border">Salut</span>
+    </div>
+  );
+}
+
+export default App;
